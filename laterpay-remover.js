94d409b89ceba91245ba0cfc5a72a6dd64c3c2f6
Michael Grundkötter <m.grundkoetter@googlemail.com>
2017-09-22 13:34:56 +0200

    Update laterpay-remover.js

--- laterpay-remover.js
+++ laterpay-remover.js
@@ -3,3 +3,9 @@ $('.article-section svg+div').removeAttr('class');
 $('.article-section svg+div+div').remove();
 SPLaterpay.callback.hasAccess();
 })();
+javascript:(function removeAdblockerNagScreen() {
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = ".ua-detected {display: none !important;} #wrapper-content {opacity: 1 !important;filter: none !important;pointer-events: auto !important;}";
+document.body.appendChild(css);
+})();
